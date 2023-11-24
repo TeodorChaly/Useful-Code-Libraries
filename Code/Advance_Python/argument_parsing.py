1d51ee59cdff381ceceaa329e563e7bcdacf7e3f
Teodors Chaly <iteo.chaly@gmail.com>
2023-11-24 23:15:44 +0200

    Exception for IDLE running argument_parsing.py

--- Code/Advance_Python/argument_parsing.py
+++ Code/Advance_Python/argument_parsing.py
@@ -3,9 +3,15 @@ import sys
 # Run next command in cmd:
 # python argument_parsing.py check.txt Hello_world!
 # In this directory file with name check.txt will appear with Hello_world! inside
+try:
+    filename = sys.argv[1]  # First argument
+    text = sys.argv[2]  # Second argument
 
-filename = sys.argv[1]  # First argument
-text = sys.argv[2]  # Second argument
+    with open(filename, "w+") as f:
+        f.write(text)
+except Exception:
+    print("Don't use IDLE for testing this. Only in CMD.")
+    print("And follow the instruction:")
+    print("Run next command in cmd")
+    print("python argument_parsing.py check.txt Hello_world!")
 
-with open(filename, "w+") as f:
-    f.write(text)
