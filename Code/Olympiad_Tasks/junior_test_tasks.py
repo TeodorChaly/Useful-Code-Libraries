3f65c7dbedfe040648bb4d61781ac349f0dddef9
Teodors Chaly <iteo.chaly@gmail.com>
2023-12-11 23:52:09 +0200

    All Junior task completed

--- Code/Olympiad_Tasks/junior_test_tasks.py
+++ Code/Olympiad_Tasks/junior_test_tasks.py
@@ -1,5 +1,49 @@
 # Tasks from https://pythonist.ru/python-voprosy-sobesedovaniya-chast-i-junior/
 
+# Task is to create a list with all prime numbers from 0 to 100
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ...]
+
+def all_prime_numbers():
+    prime_numbers = []
+    for i in range(2, 100):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            prime_numbers.append(i)
+    return prime_numbers
+
+
+# Task is to create a list with unique elements
+# [1, 2, 3, 3, 3, 3, 4, 5] -> [1, 2, 3, 4, 5]
+
+def unique_list_converter(list_with_duplicates):
+    new_list = []
+    for i in list_with_duplicates:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
+# Task is to create a string from 0 to 100
+# 123...100
+def zero_to_hundred(string):
+    for i in range(100):
+        string += str(i)
+    return string
+
+
+# Task is to delete empty strings from list
+# ["", "Hello", "", "world", ""] -> ["Hello", "world"]
+
+def delete_empty_strings(string_list):
+    new_list = []
+    for string in string_list:
+        if string != "":
+            new_list.append(string)
+    return new_list
+
+
 # Task is to convert string to ASCII
 # "Hello world!" -> [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33]
 
@@ -41,15 +85,32 @@ def binary_converter(binary_string):
 
 
 def main():
-    pass
+    print("Task 1:")
+    print(binary_converter("00001001"))
+    print(binary_converter("11111111"))
+    print("Task 2:")
 
-    # print(binary_converter("00001001"))
-    # print(binary_converter("11111111"))
+    a = (1, 2, 3)
+    b = (3, 2, 6)
+    print(tuple_checker(a, b))
+    print("Task 3:")
 
-    # a = (1, 2, 3)
-    # b = (3, 2, 6)
-    # print(tuple_checker(a, b))
+    print(string_to_ascii("Hello world!"))
+    print("Task 4:")
 
-    # print(string_to_ascii("Hello world!"))
+    string_list = ["", "Hello", "", "world", ""]
+    print(delete_empty_strings(string_list))
+    print("Task 5:")
+
+    string_from_zero_to_hundred = ""
+    print(zero_to_hundred(string_from_zero_to_hundred))
+    print("Task 6:")
+
+    list_with_duplicates = [1, 2, 3, 3, 3, 3, 4, 5]
+    print(unique_list_converter(list_with_duplicates))
+
+    print(all_prime_numbers())
+
+
 if __name__ == "__main__":
     main()
