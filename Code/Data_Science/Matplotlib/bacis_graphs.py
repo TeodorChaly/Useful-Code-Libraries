c9a48953843a05e4859612711161772d8264f142
Teodors Chaly <iteo.chaly@gmail.com>
2023-12-13 23:57:54 +0200

    Added a bar and histograms plots (matplotlib)

--- Code/Data_Science/Matplotlib/bacis_graphs.py
+++ Code/Data_Science/Matplotlib/bacis_graphs.py
@@ -18,11 +18,34 @@ def scatter_show():
 
     plt.show()  # Show the graph
 
+
 def line_plot_show():
     years = [2006 + i for i in range(16)]
     weights = [3, 5, 9, 15, 20, 25, 30, 35, 40, 45, 50, 53, 54, 55, 56, 55.6]
 
-    plt.plot(years, weights)
+    plt.plot(years, weights, lw=3, color="#000")
+    # Create a line plot:
+    # with line weight 3 (lw=3)
+
     plt.show()
 
-line_plot_show()
+
+def bar_plot_show():
+    x = ["C++", "C#", "Java", "Python", "JavaScript", "PHP"]
+    y = [20, 50, 60, 40, 30, 10]
+
+    plt.bar(x, y, color="#000", align="edge", width=0.5)
+    # Create a line plot:
+    # with align edge (align="edge")
+    # with width 0.5 (width=0.5)
+    plt.show()
+
+def histograms_plot_show():
+    ages = np.random.normal(20, 1.5, 1000)
+
+    plt.hist(ages, bins=20)
+    plt.show()
+
+
+
+histograms_plot_show()
