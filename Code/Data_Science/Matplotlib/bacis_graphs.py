55d46253ce691dd463068f4e2d69e1d1b8a6c37b
Teodors Chaly <iteo.chaly@gmail.com>
2023-12-14 18:55:59 +0200

    Added animation, multiple and 3d  plots (matplotlib)

--- Code/Data_Science/Matplotlib/bacis_graphs.py
+++ Code/Data_Science/Matplotlib/bacis_graphs.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -35,10 +37,11 @@ def bar_plot_show():
     y = [20, 50, 60, 40, 30, 10]
 
     plt.bar(x, y, color="#000", align="edge", width=0.5)
-    # Create a line plot:
+    # Create a bar plot:
     # with align edge (align="edge")
     # with width 0.5 (width=0.5)
     plt.show()
+
 
 def histograms_plot_show():
     ages = np.random.normal(20, 1.5, 1000)
@@ -47,5 +50,60 @@ def histograms_plot_show():
     plt.show()
 
 
+def pie_plot_show():
+    languages = ["C++", "C#", "Java", "Python", "JavaScript", "PHP"]
+    votes = [20, 50, 60, 40, 30, 10]
 
-histograms_plot_show()
+    plt.pie(votes, labels=languages, autopct="%1.1f%%", explode=[0, 0, 0, 0.1, 0, 0])
+    # Create a bar plot:
+    # with labels (labels=languages)
+    # with auto percentage (autopct="%1.1f%%")
+    # with explode (explode=[0, 0, 0, 0.1, 0, 0])
+    plt.show()
+
+
+def plot_customization_show():
+    year = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+    income = [55, 54, 60, 120, 151, 350, 549, 648]
+
+    plt.plot(year, income)
+    plt.title("Income per year")
+    plt.xlabel("Year")
+    plt.ylabel("Income")
+    plt.show()
+
+
+def multiple_plots_show():
+    year = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+    income = [55, 54, 60, 120, 151, 350, 549, 648]
+    expenses = [50, 60, 70, 80, 90, 100, 110, 120]
+
+    plt.plot(year, income)
+    plt.plot(year, expenses)
+    plt.title("Income and expenses per year")
+    plt.xlabel("Year")
+    plt.ylabel("Income and expenses")
+    plt.legend(["Income", "Expenses"])
+    plt.show()
+
+
+def three_d_plot_show():
+    ax = plt.axes(projection='3d')
+    x = np.random.random(100)
+    y = np.random.random(100)
+    z = np.random.random(100)
+
+    ax.scatter(x, y, z)
+    plt.show()
+
+
+def animation_plot_show():
+    head_tails = [0, 0]
+    for i in range(100):
+        head_tails[random.randint(0, 1)] += 1
+        plt.bar(["Head", "Tail"], head_tails, color=["red", "blue"])
+        plt.pause(0.05)
+    plt.show()
+
+
+animation_plot_show()
