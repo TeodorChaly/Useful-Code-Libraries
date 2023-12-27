4c2621cd0c2ad1999101f93d793b1ed8ad39df4d
Teodors Chaly <iteo.chaly@gmail.com>
2023-12-27 22:02:18 +0200

    Notification pop up function added.

--- Programs/WorkTimeTracker/app.py
+++ Programs/WorkTimeTracker/app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from time import strftime, gmtime
+from notification import PopupNotification
+
 
 class TimerApp:
     def __init__(self, root):
@@ -9,14 +11,15 @@ class TimerApp:
         self.time_label = tk.Label(root, text="00:00:00", font=("Arial", 30))
         self.time_label.pack()
 
-        self.start_button = tk.Button(root, text="Начать", command=self.start_timer)
+        self.start_button = tk.Button(root, text="GO", command=self.start_timer)
         self.start_button.pack()
 
-        self.pause_button = tk.Button(root, text="Пауза", command=self.pause_timer)
+        self.pause_button = tk.Button(root, text="PAUSE", command=self.pause_timer)
         self.pause_button.pack()
 
         self.running = False
         self.elapsed_time = 0
+        self.time_last_break = 0
 
     def start_timer(self):
         if not self.running:
@@ -28,10 +31,20 @@ class TimerApp:
 
     def update_timer(self):
         if self.running:
+            TIME_REMAINDER = 10 * 60  # Change time remainder
             self.elapsed_time += 1
             time_string = strftime('%H:%M:%S', gmtime(self.elapsed_time))
             self.time_label.config(text=time_string)
+
+            if self.elapsed_time - self.time_last_break >= TIME_REMAINDER:
+                self.show_break_notification()
+                self.time_last_break = self.elapsed_time
+
             self.root.after(1000, self.update_timer)
+
+    def show_break_notification(self):
+        PopupNotification(self.root, "It is time for little break!").show()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
