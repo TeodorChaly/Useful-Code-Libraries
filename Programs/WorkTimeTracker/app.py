78d5be4b572826c75a3c530987693d4773805349
Teodors Chaly <iteo.chaly@gmail.com>
2023-12-28 16:06:28 +0200

    Log in and registration functions was added

--- Programs/WorkTimeTracker/app.py
+++ Programs/WorkTimeTracker/app.py
@@ -1,77 +1,88 @@
 import tkinter as tk
-from time import strftime, gmtime
-from notification import PopupNotification
-from tkinter import simpledialog, messagebox
-from db_reg_log import register_user, authenticate_user
+from tkinter import Label, Entry, Button
+
+from Programs.WorkTimeTracker.db_reg_log import register_user, login_user
+from Programs.WorkTimeTracker.work_timer import TimerApp
 
 
-class TimerApp:
+class App:
     def __init__(self, root):
         self.root = root
-        root.title("Timer")
+        self.root.title("Timer App")
+        self.create_login_register_form()
 
-        self.time_label = tk.Label(root, text="00:00:00", font=("Arial", 30))
-        self.time_label.pack()
+    def create_login_register_form(self):
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
 
-        self.start_button = tk.Button(root, text="GO", command=self.start_timer)
-        self.start_button.pack()
+        self.first_name_label = Label(self.frame, text="First Name")
+        self.first_name_entry = Entry(self.frame)
+        self.last_name_label = Label(self.frame, text="Last Name")
+        self.last_name_entry = Entry(self.frame)
 
-        self.pause_button = tk.Button(root, text="PAUSE", command=self.pause_timer)
-        self.pause_button.pack()
+        self.email_label = Label(self.frame, text="Email")
+        self.email_entry = Entry(self.frame)
+        self.password_label = Label(self.frame, text="Password")
+        self.password_entry = Entry(self.frame, show='*')
+        self.submit_button = Button(self.frame, text="Submit", command=self.submit_form)
 
-        self.running = False
-        self.elapsed_time = 0
-        self.time_last_break = 0
-        self.user_authenticated = False
-        self.login_or_register()
+        self.toggle_button = Button(self.frame, text="Switch to Login", command=self.toggle_form)
 
-    def login_or_register(self):
-        if not self.user_authenticated:
-            action = simpledialog.askstring("Login/Register", "Type 'login' or 'register':")
-            email = simpledialog.askstring("Email", "Enter your email:")
-            password = simpledialog.askstring("Password", "Enter your password:", show='*')
+        self.is_registration_form = True
+        self.update_form_view()
 
-            if action == 'register':
-                # Регистрация пользователя
-                register_user(email, password, "Loh", "Odin")  # Здесь должна быть логика для регистрации пользователя
-                messagebox.showinfo("Registration", "Registration successful!")
-            elif action == 'login':
-                # Вход пользователя
-                self.user_authenticated = authenticate_user(email,
-                                                            password)  # Здесь должна быть логика для входа пользователя
-                if self.user_authenticated:
-                    messagebox.showinfo("Login", "Login successful!")
-                else:
-                    messagebox.showerror("Login", "Login failed. Please try again.")
+    def toggle_form(self):
+        self.is_registration_form = not self.is_registration_form
+        self.update_form_view()
+
+    def update_form_view(self):
+        for widget in self.frame.winfo_children():
+            widget.pack_forget()
+
+        if self.is_registration_form:
+            self.first_name_label.pack()
+            self.first_name_entry.pack()
+            self.last_name_label.pack()
+            self.last_name_entry.pack()
+            self.email_label.pack()
+            self.email_entry.pack()
+            self.password_label.pack()
+            self.password_entry.pack()
+            self.submit_button.pack()
+            self.toggle_button.config(text="Switch to Login")
+        else:
+            self.email_label.pack()
+            self.email_entry.pack()
+            self.password_label.pack()
+            self.password_entry.pack()
+            self.submit_button.pack()
+            self.toggle_button.config(text="Switch to Register")
+
+        self.toggle_button.pack()
+
+    def submit_form(self):
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+
+        if self.is_registration_form:
+            first_name = self.first_name_entry.get()
+            last_name = self.last_name_entry.get()
+            if register_user(email, first_name, last_name, password):
+                print("Registering:", first_name, last_name, email, password)
+                self.root.destroy()
+                app_root = tk.Tk()
+                TimerApp(app_root)
+        else:
+            if login_user(email, password):
+                print("Logging in:", email, password)
+                self.root.destroy()
+                app_root = tk.Tk()
+                TimerApp(app_root)
             else:
-                messagebox.showerror("Error", "Invalid action. Please type 'login' or 'register'.")
-
-    def start_timer(self):
-        if not self.running:
-            self.running = True
-            self.update_timer()
-
-    def pause_timer(self):
-        self.running = False
-
-    def update_timer(self):
-        if self.running:
-            TIME_REMAINDER = 10 * 60  # Change time remainder
-            self.elapsed_time += 1
-            time_string = strftime('%H:%M:%S', gmtime(self.elapsed_time))
-            self.time_label.config(text=time_string)
-
-            if self.elapsed_time - self.time_last_break >= TIME_REMAINDER:
-                self.show_break_notification()
-                self.time_last_break = self.elapsed_time
-
-            self.root.after(1000, self.update_timer)
-
-    def show_break_notification(self):
-        PopupNotification(self.root, "It is time for little break!").show()
+                print("Wrong email or password")
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = TimerApp(root)
+    app = App(root)
     root.mainloop()
--- Programs/WorkTimeTracker/db_reg_log.py
+++ Programs/WorkTimeTracker/db_reg_log.py
@@ -19,17 +19,20 @@ def register_user( email, first_name, last_name, password):
             cursor.execute(query, (email, first_name, last_name, hashed_password))
             connection.commit()
             print("User registered successfully")
+            return True
         except Error as e:
             print(f"The error '{e}' occurred")
+            return False
     except Error as e:
         print(f"The error '{e}' occurred")
+        return False
 
 
 def check_password(hashed_password, user_password):
     return bcrypt.checkpw(user_password.encode(), hashed_password.encode())
 
 
-def authenticate_user(email, password):
+def login_user(email, password):
     try:
         connection = create_db_connection()
         cursor = connection.cursor()
@@ -38,14 +41,15 @@ def authenticate_user(email, password):
             cursor.execute(query, (email,))
             result = cursor.fetchone()
             if result and check_password(result[0], password):
-                print("Login successful")
                 return True
             else:
                 print("Login failed")
+                return False
         except Error as e:
             print(f"The error '{e}' occurred")
 
         return False
     except Error as e:
         print(f"The error '{e}' occurred")
+        return False
 
